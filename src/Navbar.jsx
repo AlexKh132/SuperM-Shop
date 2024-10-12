ad4b252d8aeab6e49ca5a49dcc03474ecccc036f
Vigas298 <Khalizov.alex@gmail.com>
2024-10-12 16:40:35 +0200

    16.40 12.10.2024

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,10 +1,31 @@
 import { NavLink } from "react-router-dom";
+import Button from "./Button.jsx";
+import { useEffect, useState } from "react";
+
 
 export default function Navbar(props) {
+
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  const handleSetDarckTheme = () => {
+    setIsDarkTheme(!isDarkTheme);
+  };
+
   const cartCount = props.cart.reduce(
     (total, product) => total + product.quantity,
     0
   );
+
+  useEffect(() => {
+    const defaultTheme = window.matchMedia("(prefers-color-scheme: dark)").matches
+    defaultTheme && setIsDarkTheme(true);
+  }, [])
+
+
+  useEffect(() => {
+    isDarkTheme ? document.body.classList.add("dark")
+    : document.body.classList.remove("dark");
+  }, [isDarkTheme])
 
   return (
     <nav className="navbar">
@@ -13,28 +34,20 @@ export default function Navbar(props) {
       </NavLink>
       <ul>
         <li className="nav-item">
-          <NavLink
-            end
-            className={({ isActive }) => (isActive ? "active" : "")}
-            to="/"
-          >
+          <Button className="theme-switcher" onClick={handleSetDarckTheme}>{isDarkTheme ? "Dark" : "Light"}</Button>
+        </li>
+        <li className="nav-item">
+          <NavLink className={({isActive}) => isActive ? "active" : ""} to="/">
             Home
           </NavLink>
         </li>
         <li className="nav-item">
-          <NavLink
-            end
-            className={({ isActive }) => (isActive ? "active" : "")}
-            to="/about"
-          >
+          <NavLink className={({isActive}) => isActive ? "active" : ""} to="/about">
             About us
           </NavLink>
         </li>
         <li className="nav-item">
-          <NavLink
-            className={({ isActive }) => (isActive ? "active" : "")}
-            to="/products"
-          >
+          <NavLink className={({isActive}) => isActive ? "active" : ""} to="/products">
             Products
           </NavLink>
         </li>
